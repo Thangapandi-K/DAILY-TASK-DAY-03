e1d21753f1e1a428d5d8882962347b4842501134
Thangapandi-K <thangapandi.chella@gmail.com>
2024-04-18 00:22:08 +0530

    first commit

--- DISPLAY FLAGS/JS/script.js
+++ DISPLAY FLAGS/JS/script.js
@@ -3,8 +3,6 @@ const API_URL = "https://restcountries.com/v3.1/all";
 function reqListener() {
     const countriesData = JSON.parse(this.responseText);
     
-console.log(countriesData);
-
     for (let ind = 0; ind < countriesData.length; ind++) {
         console.log(countriesData[ind].flags.png);
     }
--- comparingJSON.js
+++ comparingJSON.js
@@ -1,18 +1,5 @@
-const API_URL = "https://restcountries.com/v3.1/all";
+let ob1 = { name: "Person 1", age : 5 };
+let obj2 = { age : 5, name : "Person 1"};
 
-function reqListener() {
-    const countriesData = JSON.parse(this.responseText);
-    
-console.log(countriesData);
-
-    for (let ind = 0; ind < countriesData.length; ind++) {
-        console.log(countriesData[ind].flags.png);
-    }
-  }
-  
-  
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", reqListener);
-  req.open("GET", API_URL);
-  req.send();
+console.log(ob1);
   
