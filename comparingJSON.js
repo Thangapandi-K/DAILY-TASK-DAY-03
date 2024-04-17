6854e206f1535f23a55c2a626609c0071f0dd457
Thangapandi-K <thangapandi.chella@gmail.com>
2024-04-18 00:32:58 +0530

    Task Submission

--- comparingJSON.js
+++ comparingJSON.js
@@ -1,5 +1,4 @@
-let ob1 = { name: "Person 1", age : 5 };
-let obj2 = { age : 5, name : "Person 1"};
+let obj1 = {name: "Person 1", age: 5};
+let obj2 = {age: 5, name: "Person 1"};
 
-console.log(ob1);
-  
+console.log(obj1 == obj2);
